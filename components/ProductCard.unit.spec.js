614828c24f3ca3c9adf096ba6fe1fae5f1844dde
Ramon Oliveira <ramon.oliveira17@hotmail.com>
2022-10-12 18:21:39 -0300

    feat: add method addToCart

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,40 +1,55 @@
 import { mount } from '@vue/test-utils'
 import ProductCard from '@/components/ProductCard'
 import { makeServer } from '@/miragejs/server'
+
+const mountProductCard = (server) => {
+  const product = server.create('product', {
+    title: 'Relógio bonito',
+    price: '22.00',
+    image:
+      'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80',
+  })
+
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  }
+}
+
 describe('ProductCard - unit', () => {
   let server
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' })
   })
+
   afterEach(() => {
     server.shutdown()
   })
-  it('shoud match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-          image:
-            'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80',
-        }),
-      },
-    })
 
+  it('shoud match snapshot', () => {
+    const { wrapper } = mountProductCard(server)
     expect(wrapper.element).toMatchSnapshot()
   })
-  it('Component mount', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-        }),
-      },
-    })
+
+  it('should mount the component', () => {
+    const { wrapper } = mountProductCard(server)
     expect(wrapper.vm).toBeDefined()
     expect(wrapper.text()).toContain('Relógio bonito')
     expect(wrapper.text()).toContain('$22.00')
+  })
+
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard(server)
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.emitted().addToCart).toBeTruthy()
+    expect(wrapper.emitted().addToCart.length).toBe(1)
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
   })
 })
