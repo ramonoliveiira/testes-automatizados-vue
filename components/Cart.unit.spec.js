c4ac8d7352e0490313bbc3be61fd46e39d7017c7
Ramon Oliveira <ramon.oliveira17@hotmail.com>
2022-11-26 00:38:04 -0300

    feat: Recebendo products via props em CartItem

--- components/Cart.unit.spec.js
+++ components/Cart.unit.spec.js
@@ -1,7 +1,19 @@
 import { mount } from '@vue/test-utils'
 import Cart from '@/components/Cart'
+import CartItem from '@/components/CartItem'
+import { makeServer } from '@/miragejs/server'
 
 describe('Cart - unit', () => {
+  let server
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' })
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
   it('should mount the component', () => {
     const wrapper = mount(Cart)
 
@@ -38,5 +50,17 @@ describe('Cart - unit', () => {
     const wrapper = mount(Cart)
 
     expect(wrapper.text()).toContain('Cart is empty')
+  })
+
+  it('should display 2 instances of CartItem when 2 products are provided', () => {
+    const products = server.createList('product', 2)
+    const wrapper = mount(Cart, {
+      propsData: {
+        products,
+      },
+    })
+
+    expect(wrapper.findAllComponents(CartItem)).toHaveLength(2)
+    expect(wrapper.text()).not.toContain('Cart is empty')
   })
 })
