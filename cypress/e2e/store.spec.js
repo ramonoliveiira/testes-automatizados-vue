9e6d0d5fd16496dda7bcf947fa2055c9a1e1c5b3
Ramon Oliveira <raomartins@uneb.br>
2023-06-28 23:18:22 -0300

    Finalizando Aula 52:  adicionando novo custom custom cypress command chamado 'getByTestId'

--- cypress/e2e/store.spec.js
+++ cypress/e2e/store.spec.js
@@ -5,6 +5,7 @@ import { makeServer } from '../../miragejs/server'
 context('Store', () => {
   let server
   const g = cy.get
+  const gid = cy.getByTestId
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' })
@@ -26,36 +27,36 @@ context('Store', () => {
     it('should not display shopping cart when page first loads', () => {
       cy.visit('/')
 
-      g('[data-testid="shopping-cart"]').should('have.class', 'hidden')
+      gid('shopping-cart').should('have.class', 'hidden')
     })
     it('should toggle shopping cart visibility when button is clicked', () => {
       cy.visit('/')
-      g('[data-testid="toggle-button"]').as('toggleButton')
+      gid('toggle-button').as('toggleButton')
       g('@toggleButton').click()
-      g('[data-testid="shopping-cart"]').should('not.have.class', 'hidden')
+      gid('shopping-cart').should('not.have.class', 'hidden')
       g('@toggleButton').click({ force: true })
-      g('[data-testid="shopping-cart"]').should('have.class', 'hidden')
+      gid('shopping-cart').should('have.class', 'hidden')
     })
   })
 
   context('Store > Product List', () => {
     it('should display "0 Products" when no product is returned', () => {
       cy.visit('/')
-      g('[data-testid="product-card"').should('have.length', 0)
+      gid('product-card').should('have.length', 0)
       g('body').contains('0 Products')
     })
     it('should display "1 Product" when 1 product is returned', () => {
       server.create('product')
 
       cy.visit('/')
-      g('[data-testid="product-card"').should('have.length', 1)
+      gid('product-card').should('have.length', 1)
       g('body').contains('1 Product')
     })
     it('should display "10 Products" when 10 products are returned', () => {
       server.createList('product', 10)
 
       cy.visit('/')
-      g('[data-testid="product-card"').should('have.length', 10)
+      gid('product-card').should('have.length', 10)
       g('body').contains('10 Products')
     })
   })
@@ -74,16 +75,16 @@ context('Store', () => {
 
       cy.visit('/')
       g('input[type="search"]').type('Relógio bonito')
-      g('[data-testid="search-form"]').submit()
-      g('[data-testid="product-card"').should('have.length', 1)
+      gid('search-form').submit()
+      gid('product-card').should('have.length', 1)
     })
     it('should not return any product', () => {
       server.createList('product', 10)
 
       cy.visit('/')
       g('input[type="search"]').type('Relógio bonito')
-      g('[data-testid="search-form"]').submit()
-      g('[data-testid="product-card"').should('have.length', 0)
+      gid('search-form').submit()
+      gid('product-card').should('have.length', 0)
       g('body').contains('0 Products')
     })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/* Criando comando personalizado */
+Cypress.Commands.add('getByTestId', (selector) => {
+  return cy.get(`[data-testid="${selector}"]`)
+})
