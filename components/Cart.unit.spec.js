77792d7d43fa06db467990044da42815e59f931b
Ramon Oliveira <raomartins@uneb.br>
2023-07-19 23:04:15 -0300

    Finalizando Aula 57

--- components/Cart.unit.spec.js
+++ components/Cart.unit.spec.js
@@ -37,6 +37,20 @@ describe('Cart - unit', () => {
     expect(wrapper.vm).toBeDefined()
   })
 
+  it('should not display empty cart button when there are no products', () => {
+    const { cartManager } = mountCart()
+
+    const wrapper = mount(Cart, {
+      mocks: {
+        $cart: cartManager,
+      },
+    })
+
+    expect(wrapper.find('[data-testid="clear-cart-button"]').exists()).toBe(
+      false
+    )
+  })
+
   it('should emit close event when button gets clicked', async () => {
     const { wrapper } = mountCart()
     const button = wrapper.find('[data-testid="close-button"]')
